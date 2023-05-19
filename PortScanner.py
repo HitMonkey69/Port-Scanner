944b259711c92ad87c308295b06346ba11dd0bfe
HitMonkey69 <romesh150998@gmail.com>
2023-05-19 14:04:29 +0530

    First commit

---
+++ PortScanner.py
@@ -0,0 +1,25 @@
+# #Port Scanner
+
+
+# import socket
+# import pyfiglet
+
+# banner = pyfiglet.figlet_format('Port Scanner')
+# print(banner)
+# print('Enter 1 to use Port Scanner')
+# print('Enter 0 to Quit')
+
+# while(True):
+#     usr = int(input('Enter your Choice:'))
+
+#     if usr == 0:
+#         break
+
+#     else:
+    
+
+#         host = input('Enter Host to Scan:')
+#         hostIP = socket.gethostbyname(host)
+
+#         print(hostIP)
+
